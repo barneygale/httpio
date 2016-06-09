9767bcc085e0f6b3225cf14e52a2f034ff6fb767
Barney Gale <barney.gale@gmail.com>
2016-06-10 00:40:56 +0100

    Bump version to 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='httpio',
-    version='0.1.2',
+    version='0.1.3',
     author='Barney Gale',
     author_email='barney@barneygale.co.uk',
     url='https://github.com/barneygale/httpio',
